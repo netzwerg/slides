5e5b141f90e972eadc2e6d890ba3a2643195c4a2
Rahel Lüthy <rahel.luethy@gmail.com>
2016-02-06 08:23:33 +0100

    Elaborate 'We Can Do Better!'

--- src/main/java/ch/netzwerg/javaslang/Snippets.java
+++ src/main/java/ch/netzwerg/javaslang/Snippets.java
@@ -3,20 +3,31 @@ package ch.netzwerg.javaslang;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 public class Snippets {
 
     private static final String CONSTANT = "foo";
     private final List<String> state = new ArrayList<String>();
+    private final List<String> moreState = new ArrayList<String>();
 
     // tag::input-output[]
     List<String> doSomething(List<String> list) {
-        if (this.state.isEmpty()) {
+        if (this.state.isEmpty() || list.isEmpty()) {
             throw new IllegalStateException("Ouch...");
         }
+
         list.add(this.state.get(0));
         list.add(CONSTANT);
+
+        this.moreState.addAll(list);
+        OtherClass.mutableStaticField = list;
+
         return new ArrayList<String>(list);
     }
     // end::input-output[]
+
+    private static class OtherClass {
+        static List<String> mutableStaticField;
+    }
 
 }
